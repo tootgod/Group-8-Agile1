042d7781aaf52d1b89b6c66b86b3cf9e89a63b8b
itsnickweber <nicholas.m.weber123@gmail.com>
2020-08-31 00:05:05 -0700

    Added Basic Functionality

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,61 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord); // Debug Line
+    
+    // Welcoming Our Player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press enter to continue..."));
+
+    
+
+    // Prompt Player for Guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    
+    HiddenWord.Len();
 
+    // Checking PlayerGuess
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters long, try again!"), HiddenWord.Len());
+        }
+        
+
+        PrintLine(TEXT("You have lost!"));
+    }
+    // Check if Isogram
+    // Prompt to guess again
+    // Check right number of characters
+    // Prompt to guess again
+
+    // Remove Life
+
+    // Check if Lives > 0
+    // If Yes, GuessAgain
+    // Show Lives Left
+    // If No, Show GameOever and HiddenWord
+    // Prompt to Play Again, "Press Enter to Play Again?"
+    // Check their Input
+    // PlayAgain or Quit
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cakes");
+    Lives = 4;
 }
